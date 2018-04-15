f37d9375b226ab39fed4dbd76362acb4f1c39452
Daniel Byrne <danwbyrne@gmail.com>
2018-04-15 00:07:10 -0400

    holding of on Tkinter GUI
    taking a break here, will come back

--- src/main.py
+++ src/main.py
@@ -1,31 +1,59 @@
 import screenGrab
+import tkinter as tk
+import numpy as np
 import pyhue
 import time
 import json
 
-class App():
+def RGBtoHEX(rgb):
+	return "#" + ''.join( ['%X' % int(value) for value in rgb])
 
-	def __init__(self):
+class Application(tk.Frame):
 
+	def __init__(self, master=None):
+		super().__init__(master)
 		self.config = None
 
-
-
-
-		loadConfig()
+		self.pack()
+		self.loadConfig()
+		self.create_widgets()
 
 	def loadConfig(self):
 
 		with open('config.json', 'rb') as cfg_file:
 			self.config = json.load(cfg_file)
 
-	def test(self):
+	def create_widgets(self):
+		self.start_button = tk.Button(self)
 
-		return speedTest(self.config['monitor'])
+		self.start_button['text']    = "Click to start HueCtrl"
+		self.start_button['command'] = self.run()
+		self.start_button.pack(side = "top")
+
+	def getColor(self):
+		color  = np.zeros(3)
+		screen = screenGrab.getPixels( self.config['monitor'] )
+		shape  = screen.shape[:2]
+		num    = shape[0]*shape[1]
+
+		for y in range(shape[0]):
+			for x in range(shape[1]):
+
+				color = np.add(color, screen[y,x])
+
+		return np.divide(color, num)
+
+	def run(self):
+	
+		self.start_button.config( bg = RGBtoHEX(self.getColor()) )
+
+
+
+
+
 
 
 def speedTest(monitor):
-
 	return sum([pixelTest(monitor) for i in range(100)])/100.
 
 def pixelTest(monitor):
@@ -34,8 +62,9 @@ def pixelTest(monitor):
 	return time.time()-start
 
 def main():
-	ctrl = App()
-	print(ctrl.test())
+	root = tk.Tk()
+	ctrl = Application()
+	ctrl.mainloop()
 
 if __name__ == "__main__":
 	main()
